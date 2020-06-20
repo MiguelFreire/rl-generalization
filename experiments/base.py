4fb72fd40e98c12417fb18e7e2938267c764587b
Miguel Freire <m.b.freire@hotmail.com>
2020-06-20 06:02:47 +0100

    Fixes bug

--- experiments/base.py
+++ experiments/base.py
@@ -1,7 +1,7 @@
 from experiments.experiment import Experiment
 
 class BaseExperimentNature(Experiment):
-    def __init__(self, name="CoinRunNatureLevels-default", num_levels=500, batchNorm=False, dropout=0.0, l2_penalty=0, entropy_bonus = 0.01, augment_obs=None, attention=None, self.num_steps=25_000_000):
+    def __init__(self, name="CoinRunNatureLevels-default", num_levels=500, batchNorm=False, dropout=0.0, l2_penalty=0, entropy_bonus = 0.01, augment_obs=None, attention=None, num_steps=25_000_000):
       self.num_levels = num_levels
       self.batchNorm = batchNorm
       self.dropout = dropout
@@ -10,7 +10,7 @@ class BaseExperimentNature(Experiment):
       self.entropy_bonus = entropy_bonus
       self.augment_obs = augment_obs
       self.attention = attention
-      self.num_steps
+      self.num_steps = num_steps
     def getConfig(self):
         return {
             "name": self.name,
