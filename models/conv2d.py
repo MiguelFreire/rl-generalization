9207433e60527a0317e2b41839ff40044d71c611
Miguel Freire <m.b.freire@hotmail.com>
2020-11-25 02:26:44 +0000

    resnet test

--- models/conv2d.py
+++ models/conv2d.py
@@ -72,20 +72,21 @@ class Conv2dResModel(torch.nn.Module):
     def __init__(self, in_channels, layers=2, use_maxpool=False):
         super().__init__()
         seq = list()
-        conv1 = torch.nn.Conv2d(in_channels=in_channels, out_channels=32,
-            kernel_size=8, stride=4, padding=0)
+        conv1 = torch.nn.Conv2d(in_channels=in_channels, out_channels=16,
+            kernel_size=4, stride=1, padding=0)
         seq.append(conv1)
         
         if use_maxpool:
-          max_pool = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+          max_pool = torch.nn.MaxPool2d(2)
           seq.append(max_pool)
         
-        resBlock = ResidualBlock(in_channels=32, channels=64, layers=layers, kernel_size=3, stride=1, padding=1, useConv1x1=True)
-        resBlock2 = ResidualBlock(in_channels=64, channels=64, layers=layers, kernel_size=3, stride=1, padding=1)
+        resBlock = ResidualBlock(in_channels=16, channels=32, layers=layers, kernel_size=3, stride=1, padding=0, useConv1x1=True)
+        resBlock2 = ResidualBlock(in_channels=32, channels=64, layers=layers, kernel_size=3, stride=1, padding=1)
         
         seq.append(resBlock)
+        seq.append(torch.nn.MaxPool2d(2))
         seq.append(resBlock2)
-        
+        seq.append(torch.nn.MaxPool2d(2))
         
         self.conv = torch.nn.Sequential(*seq)
 
